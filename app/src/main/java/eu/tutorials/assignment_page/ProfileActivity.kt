f64a98c0a840b930a797389ca57345b3189a0631
krishrathi08 <rathikrish539@gmail.com>
2025-05-26 00:45:56 +0530

    First Commit

---
+++ app/src/main/java/eu/tutorials/assignment_page/ProfileActivity.kt
@@ -0,0 +1,11 @@
+package eu.tutorials.assignment_page
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
